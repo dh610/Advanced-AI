0d17a64c8014279404f9d9c71b2cce2ebc80300c
dh610 <ddhhy@LAPTOP-QTT02M69>
2024-09-23 15:55:56 +0900

    move train.py

--- Restormer/train.py
+++ Restormer/train.py
@@ -317,4 +317,5 @@ def main():
 
 
 if __name__ == '__main__':
+    print(sys.path)
     main()
--- Restormer/train.sh
+++ Restormer/train.sh
@@ -2,4 +2,4 @@
 
 CONFIG=$1
 
-python -m torch.distributed.launch --nproc_per_node=8 --master_port=4321 basicsr/train.py -opt $CONFIG --launcher pytorch
+python3 -m torch.distributed.launch --nproc_per_node=8 --master_port=4321 train.py -opt $CONFIG --launcher pytorch
